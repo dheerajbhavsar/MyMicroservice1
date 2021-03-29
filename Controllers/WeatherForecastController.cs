fd60c023f567debc60c16f5d1fdfa8ab321c0686
Dhiraj Bhavsar <dheeraj.89@live.com>
2021-03-29 15:21:36 +0000

    Added message to WeatherForecast

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -31,7 +31,8 @@ namespace MyMicroService1.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Message = "Welcome to Kubectl CI-CD"
             })
             .ToArray();
         }
